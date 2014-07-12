1764220ddcacb65803908eccdb1619cac81fdd5c
kartik5 <sharma.k51@gmail.com>
2014-07-12 18:25:53 +0530

    Create Plot3.R

---
+++ Plot3.R
@@ -0,0 +1,37 @@
+# plot2.R - Histogram of Global Active Power
+
+#Reading the data into R
+
+data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";",na.strings = "?",colClasses = c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
+
+#Creating Datetime variable
+data$Time <- paste(data$Date, data$Time,sep = " ")
+
+#converting the Date and Time variables to Date/Time classes
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+data$Time <- strptime(data$Time, format = "%d/%m/%Y %H:%M:%S")
+
+#Subset the data for observation of 01 and 02 Feb 2007
+
+dataUse <- subset(data, data$Date == as.Date("2007-02-01") | data$Date == as.Date("2007-02-02"))
+
+
+# Open plot3.pngstr
+png("plot3.png", height=480, width=480)
+
+# Build time series
+plot(data$DateTime, 
+     data$Sub_metering_1, 
+     pch=NA, 
+     xlab="", 
+     ylab="Energy sub metering")
+lines(data$DateTime, data$Sub_metering_1)
+lines(data$DateTime, data$Sub_metering_2, col='red')
+lines(data$DateTime, data$Sub_metering_3, col='blue')
+legend('topright', 
+       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       lty = c(1,1,1),
+       col = c('black', 'red', 'blue'))
+
+# Close PNG file
+dev.off()
